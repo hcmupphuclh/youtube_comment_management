09616a27954cab24fea0d308baff4238a5b8c95c
Hạo Thiền <hcmup.phuclh@gmail.com>
2024-07-05 18:11:56 +0700

    change
    Change folder from diagrams to blueprints

---
+++ resources/blueprints/diagrams/endpoints/home/graphOfDataRequestion.py
@@ -0,0 +1,31 @@
+from os.path import abspath
+from flask import request, Response
+import jsonpickle
+
+from resources.applications.protocols.blueprints_main_stub import ProgressOfMainStub
+from resources.abstractions.endpoints import AbstractionOfEndpoints
+from resources.blueprints.main.attachments.diagrams.configs.APIRequestionFromFiles import AttachmentOfBlueprints_APIRequestionFromFiles
+from resources.blueprints.main.attachments.diagrams.programs.YoutubeAPIRequestion import YoutubePortion
+
+
+class AttachmentOfBlueprints_DataRequestionEndpointGraph(AbstractionOfEndpoints):
+    
+    def __init__(self, progressOfMainStub: ProgressOfMainStub) -> None:
+        super().__init__(progressOfMainStub)
+        
+        self.route = "/dataReceivement"
+        self.endpoint = "DataReceivement"
+        self.callback_function = self.get_data
+        self.methods = ["POST", "GET"]
+        
+    def get_data(performanceChosenAccount):
+        pathToChosenClientSecretFolder = abspath("resources/blueprints/main/attachments/diagrams/client_secret_files/performance/")
+        jsonData = request.get_json()
+        
+        # youtubeAPIRequestion = APIsOfYoutubeFromClientSecret(pathToChosenClientSecretFolder+"/"+jsonData["performanceChosenAccount"])
+        youtubeAPIRequestion = AttachmentOfBlueprints_APIRequestionFromFiles(pathToChosenClientSecretFolder+"/"+jsonData["performanceChosenAccount"])
+        youtubePortion = YoutubePortion(youtubeAPIRequestion.core)
+    
+        results = youtubePortion.getAllItems()
+        
+        return Response(jsonpickle.encode(results), mimetype="application/json")
