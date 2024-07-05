d7cdda6d0bd4f9e02ad82bccafee660f1afe0f76
Hạo Thiền <hcmup.phuclh@gmail.com>
2024-07-05 18:17:47 +0700

    Update index_diagram.py

--- resources/blueprints/diagrams/index_diagram.py
+++ resources/blueprints/diagrams/index_diagram.py
@@ -1,10 +1,7 @@
 from resources.abstractions.blueprints import AbstractionOfBlueprints
-from resources.blueprints.main.attachments.diagrams.endpoints.home.graphOfHomeEndpoint import AttachmentOfBlueprints_HomeEndpointGraph
-from resources.blueprints.main.attachments.diagrams.endpoints.home.graphOfDataRequestion import AttachmentOfBlueprints_DataRequestionEndpointGraph
-from resources.blueprints.main.attachments.diagrams.endpoints.home.graphOfCommentExecution import AttachmentOfBlueprints_CommentExecutionEndpointGraph
-from resources.blueprints.main.attachments.diagrams.endpoints.upload.performancefiles_upload import UploadHandleForPerformance
-from resources.blueprints.main.attachments.diagrams.endpoints.upload.audiancefiles_upload import UploadHandleForAudiance
-
+from resources.blueprints.diagrams.endpoints.home.graphOfHomeEndpoint import AttachmentOfBlueprints_HomeEndpointGraph
+from resources.blueprints.diagrams.endpoints.home.graphOfDataRequestion import AttachmentOfBlueprints_DataRequestionEndpointGraph
+from resources.blueprints.diagrams.endpoints.home.graphOfCommentExecution import AttachmentOfBlueprints_CommentExecutionEndpointGraph
 
 class AttachmentOfBlueprints_IndexDiagram(AbstractionOfBlueprints):
     
@@ -12,8 +9,8 @@ class AttachmentOfBlueprints_IndexDiagram(AbstractionOfBlueprints):
         
         self.name = "main"
         self.import_name = "__name__"
-        self.template_folder = "resources/blueprints/main/attachments/diagrams/endpoints/templates"
-        self.static_folder = "resources/blueprints/main/attachments/diagrams/endpoints/templates/static"
+        self.template_folder = "resources/blueprints/attachments/templates"
+        self.static_folder = "resources/blueprints/attachments/templates/static"
         
         self.blueprint_adjustment()
         self.endpoints_adjustment()
@@ -21,12 +18,6 @@ class AttachmentOfBlueprints_IndexDiagram(AbstractionOfBlueprints):
     def endpoints_adjustment(self):
         home = AttachmentOfBlueprints_HomeEndpointGraph(self)
         home.endpoint_adjustment()
-        
-        performanceUploadHandler = UploadHandleForPerformance(self)
-        performanceUploadHandler.endpoint_adjustment()
-        
-        audianceUploadHandler = UploadHandleForAudiance(self)
-        audianceUploadHandler.endpoint_adjustment()
         
         dataRequestionHandler = AttachmentOfBlueprints_DataRequestionEndpointGraph(self)
         dataRequestionHandler.endpoint_adjustment()
